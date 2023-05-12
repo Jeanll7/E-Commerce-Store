f86a691343aacd07ab42e638de04297a6f5969c0
devjean <jeancarloleall@gmail.com>
2023-05-12 16:17:03 -0300

    products-box

---
+++ src/app/pages/home/components/products-box/products-box.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-box',
+  template: `
+    <p>
+      products-box works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ProductsBoxComponent {
+
+}
