6d26727a332f9155181e010950671774e9e2dbee
devjean <jeancarloleall@gmail.com>
2023-05-12 00:57:38 -0300

    filters

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styles: [],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  cols = 3;
+  category: string | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
+}
