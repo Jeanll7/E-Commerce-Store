2d91ab30bdfd5503964579c965495d9d58ade7bf
devjean <jeancarloleall@gmail.com>
2023-05-14 14:40:03 -0300

    filters

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 export class FiltersComponent implements OnInit {
   @Output() showCategory = new EventEmitter<string>();
 
-  categories = ["shoes", "sports"];
+  categories = ["sapato", "esportes"];
 
   constructor() {}
 
