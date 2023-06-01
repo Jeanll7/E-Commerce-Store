9946bf828e161b7536c31188ff7c6ed132182129
devjean <jeancarloleall@gmail.com>
2023-05-31 23:45:18 -0300

    category

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -6,6 +6,9 @@ import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 })
 export class ProductsHeaderComponent implements OnInit {
   @Output() columnsCountChange = new EventEmitter<number>();
+  @Output() itemsCountChange = new EventEmitter<number>();
+  @Output() sortCountChange = new EventEmitter<string>();
+
   sort = "desc";
   itemsShowCount = 12;
 
@@ -15,10 +18,12 @@ export class ProductsHeaderComponent implements OnInit {
 
   onSortUpdated(newSort: string): void {
     this.sort = newSort;
+    this.sortCountChange.emit(newSort);
   }
 
   onItemsUpdated(count: number): void {
     this.itemsShowCount = count;
+    this.itemsCountChange.emit(count);
   }
 
   onColumnsUpdated(colsNum: number): void {
